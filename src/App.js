0d463c42ee63ebb01a2c53154d1c5a6ba3e86655
raju-ahammad <web.rajuahammad@gmail.com>
2020-11-07 21:33:39 +0600

    setup react router dom

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
-import { Button } from '@chakra-ui/core';
+import React from 'react';
+import { Route, Switch } from 'react-router-dom';
 import './App.css';
-
 function App() {
   return (
     <div className="App">
-      <Button>Hello</Button>
+      <Switch>
+        <Route exact path="/">Post Lis 0</Route>
+        <Route exact path="/posts">Post List 1</Route>
+        <Route path="/posts/new"> New Post</Route>
+        <Route path="/posts/:postId">Post Id</Route>
+      </Switch>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,13 +1,16 @@
 import { ThemeProvider } from '@chakra-ui/core';
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
 import App from './App';
 
 
 ReactDOM.render(
   <React.StrictMode>
     <ThemeProvider>
-    <App />
+      <Router>
+       <App />
+      </Router>
     </ThemeProvider>
   </React.StrictMode>,
   document.getElementById('root')
